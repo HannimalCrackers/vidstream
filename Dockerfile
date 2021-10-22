073a541e0606fdae802746f39ca20c14310b5ab4
billmills <you@example.com>
2021-10-22 18:23:03 -0400

    full processing cycle

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM python:3.7
 
-RUN pip install flask
+RUN pip install flask scikit-image
 
 WORKDIR /app
+RUN mkdir static
 COPY fleshy.py .
-COPY templates/demo.html templates/demo.html
+COPY templates templates
 ENV FLASK_APP=fleshy
 CMD flask run --host=0.0.0.0
--- fleshy.py
+++ fleshy.py
@@ -1,13 +1,38 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import base64, numpy, os
+from skimage import data, io, filters
+ 
+UPLOAD_FOLDER = '/pics'
 
 # creates a Flask application, named app
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
 def hello():
     return render_template('demo.html')
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    image_b64 = request.form['demo'].split(',')[1]
+    image_data = base64.b64decode(image_b64)
+    with open("test.png", "wb") as fo:
+       fo.write(image_data)
+
+    imageprocess()
+
+    return render_template('result.html')
+
+
+def imageprocess(name='test.png'):
+    #filename = os.path.join(skimage.data_dir, name)
+    monster = io.imread(name)
+    edges = filters.sobel(monster)
+    io.imsave('static/monster.png', edges)
+
 
 # run the application
 if __name__ == "__main__":
